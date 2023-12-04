2ffda9bef90330789400a007738c3dff15716001
jwoong1109 <72911353+jwoong1109@users.noreply.github.com>
2023-12-04 17:13:19 +0900

    리액트 헤더 완료

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,9 +1,67 @@
-import React from 'react'
+import React, { useState } from "react";
+
+const headerNav = [
+  {
+      title: "intro",
+      url: "#intro"
+  },
+  {
+      title: "skill",
+      url: "#skill"
+  },
+  {
+      title: "site",
+      url: "#site"
+  },
+  {
+      title: "portfolio",
+      url: "#port"
+  },
+  {
+      title: "contact",
+      url: "#contact"
+  }
+];
 
 const Header = () => {
-  return (
-    <div>Header</div>
-  )
-}
+    const [show, setShow] = useState(false);
+    
+    const toggleMenu = () => {
+        setShow((prevShow) => !prevShow) //true가 되면 false가 되고, false가 되면 true가 되어라
+    }
 
-export default Header
+    return (
+        <header id="header" role="banner">
+            <div className="header__inner">
+                <div className="header__logo">
+                    <a href="/">portfolio<em>react.js</em></a>
+                </div>
+                <nav
+                    className={`header__nav ${show ? "show" : ""}`}
+                    role="navigation"
+                    aria-label="메인메뉴">
+                    <ul>
+                       {headerNav.map((nav, key) => ( //nav는 내맘대로 지정한 변수
+                        <li key={key}>
+                          <a href={nav.url}>{nav.title}</a>
+                        </li>
+                       ))}
+                    </ul>
+                </nav>
+                <div 
+                    className="header__nav__mobile" 
+                    id="headerToggle" 
+                    aria-controls="primary-menu" 
+                    aria-expanded={show ? "true" : "false"} 
+                    role="button"  
+                    tabIndex="0"
+                    onClick={toggleMenu}
+                >
+                    <span></span>
+                </div>
+            </div>
+        </header>
+    );
+};
+
+export default Header;
