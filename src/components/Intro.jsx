2fef2019cd96381f15d5963531b227eb22050ce3
jwoong1109 <72911353+jwoong1109@users.noreply.github.com>
2023-12-04 17:41:14 +0900

    인트로 화면

--- src/components/Intro.jsx
+++ src/components/Intro.jsx
@@ -1,9 +1,48 @@
-import React from 'react'
+import React from "react";
 
-const Intro = () => {
-  return (
-    <div>Intro</div>
-  )
+import about from "../assets/img/about.png";
+
+const introText = {
+  title:"port developer",
+  desc: ["talent is", "found at the end of the","effort"]
 }
 
-export default Intro
+const Intro = () => {
+    return (
+        <section id="intro">
+            <div class="intro__inner">
+                <h1 class="intro__title">{introText.title}</h1>
+                <div class="intro__lines" aria-hidden="true">
+                    <span class="line"></span>
+                    <span class="line"></span>
+                    <span class="line"></span>
+                    <span class="line"></span>
+                    <span class="line"></span>
+                    <span class="line"></span>
+                    <span class="line"></span>
+                </div>
+                <div class="intro__text">
+                    <div class="text">
+                        <div>{introText.desc[0]}</div>
+                        <div>{introText.desc[1]}</div>
+                        <div>{introText.desc[2]}</div>
+                    </div>
+                    <div class="img">
+                        <img src={about} alt="어바웃" />
+                    </div>
+                </div>
+                <div class="intro__lines bottom" aria-hidden="true">
+                    <span class="line"></span>
+                    <span class="line"></span>
+                    <span class="line"></span>
+                    <span class="line"></span>
+                    <span class="line"></span>
+                    <span class="line"></span>
+                    <span class="line"></span>
+                </div>
+            </div>
+        </section>
+    );
+};
+
+export default Intro;
